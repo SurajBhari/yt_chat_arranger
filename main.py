d0b97fbc3497871cac593ba98f607d992cb09f3a
AG4lyf <surajbhari159@gmail.com>
2022-08-25 15:59:58 +0530

    seperate video gainer to prevent connection closing on long streams

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from chat_downloader import ChatDownloader, errors
 import scrapetube
 
 from os import listdir
+import json
 
 channel_id = 'UCIzzPhdRf8Olo3WjiexcZSw'
 #channel_id = str(input("Enter Channel ID"))
@@ -9,24 +10,37 @@ channel_id = 'UCIzzPhdRf8Olo3WjiexcZSw'
 
 videos = scrapetube.get_channel(channel_id)
 
+vids = []
 for video in videos:
+    vids.append(video)
+    print("{} - {}".format(video['title'], video['url']))
+
+for video in vids:
     id = video['videoId']
     if id+".txt" in listdir("./chats_storage"):
-        print(f"Skipping {id} cuz it already exists")
-        continue
+        if id+'.json' in listdir("./json_storage"):
+            print(f"Skipping {id} cuz it already exists")
+            continue
+    
     print(f"Looking at video id - {video['videoId']}")
     try:
         chat = ChatDownloader().get_chat(video['videoId'])
     except errors.NoChatReplay:
         continue
     string = ""
+    messages = {'messages':[]}
     for message in chat:
         try:
             print(message['message'])
             string += f"{message['timestamp']} | {message['time_in_seconds']} | {message['time_text']} | {message['author']['name']} | {message['message']}\n"
+            messages['messages'].append(message)
         except Exception as e:
             print(e)
 
     with open(f'chats_storage/{id}.txt', "w+", encoding='utf-8') as f:
         f.write(string)
+    
+    with open(f'json_storage/{id}.json', "w+", encoding='utf-8') as f:
+        json.dump(messages, f, indent=4)
+    
         
