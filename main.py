a262dcd84f6b97f118dbf465cfce5e3efa6f24f5
AG4lyf <surajbhari159@gmail.com>
2022-08-25 15:09:21 +0530

    optimize file writing to only write once

--- main.py
+++ main.py
@@ -12,10 +12,14 @@ for video in videos:
     id = video['videoId']
     print(f"Looking at video id - {video['videoId']}")
     chat = ChatDownloader().get_chat(video['videoId'])
-    
-    with open(f'chats_storage/{id}.txt', "w+") as f:
-        for message in chat:
-            try:
-                f.write(f"{message['timestamp']} | {message['time_in_seconds']} | {message['time_text']} | {message['author']['name']} | {message['message']}\n")
-            except Exception as e:
-                print(e)
+    string = ""
+    for message in chat:
+        try:
+            print(message['message'])
+            string += f"{message['timestamp']} | {message['time_in_seconds']} | {message['time_text']} | {message['author']['name']} | {message['message']}\n"
+        except Exception as e:
+            print(e)
+
+    with open(f'chats_storage/{id}.txt', "w+", encoding='utf-8') as f:
+        f.write(string)
+        
