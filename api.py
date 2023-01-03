61a6f51922e3a3664498b67a0c1079658cc1b278
AG4lyf <surajbhari159@gmail.com>
2023-01-03 20:45:17 +0530

    fix shit

--- api.py
+++ api.py
@@ -3,27 +3,36 @@ from flask import Flask, request
 from json import load
 app = Flask(__name__)
 from os import listdir
+
+import json
+from urllib.parse import parse_qs
+
+
     
+@app.get("/")
+def main():
+    return "hello world"
 
-secret_key = "pass123"
-
-@app.get('/stats/<channel_id>/<user_id>')
-def stats(channel_id, user_id):
-    password = ""
+@app.get('/stats/')
+def stats():
     try:
-        password = request.headers['secret_key']
+        channel = parse_qs(request.headers["Nightbot-Channel"])
+        user = parse_qs(request.headers["Nightbot-User"])
     except KeyError:
-        password = ""
-    if password != secret_key:
-        return "INVALID KEY"
+        return "Not able to auth"
+
+    channel_id = channel.get("providerId")[0]
+    user_id = user.get("providerId")[0]
+
+    if channel_id not in listdir("."):
+        return "Channel not cached (yet)"
     with open(f"./{channel_id}/first_ever.json", mode="r", encoding="utf-8") as f:
         data = load(f)
-    try:
-        user = data[user_id]
-    except KeyError:
-        return "No Data Found."
     with open(f"./{channel_id}/person_wise/{user_id}.txt", mode="r", encoding="utf-8") as f:
         count = len(f.readlines())
-    return f"{user['name']} You have made {count} messages, You First interacted with us on a stream that was {user['ago']}, here -> {user['link']}"
+    print("Here")
+    user_data = data[user_id]
+    print(user_data)
+    return f"{user_data['name']} You have made {count} messages, You First interacted with us on a stream that was {user_data['ago']}, here -> {user_data['link']}"
 
-app.run(port=5012)
+app.run(port=5000, host="0.0.0.0")
